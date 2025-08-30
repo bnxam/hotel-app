7ae1934021af43e72728a7f81206885bd4aeea85
bnxam <bensekhriasma@gmail.com>
2025-08-30 05:47:00 +0100

    metre tout en place pour commancer

---
+++ front/src/componant/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import acceuil from '../assets/img/acceuil.jpg';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="" >
+                <p className="text-4">hallo world</p>
+            </nav>
+            <main>
+
+            </main>
+        </>
+    );
+}
+
+export default Navbar;
--- front/src/pages/acceuil.jsx
+++ front/src/pages/acceuil.jsx
@@ -1,10 +1,15 @@
-import react from react ;
+import React from "react" ;
+import Navbar from "../componant/navbar";
 
 const Acceuil = () =>{
     return(
+        // <h1>helloooo</h1>
         <div>
-            
+           
+        <Navbar></Navbar>
         </div>
     );
 
 }
+
+export default Acceuil;
